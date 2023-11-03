bfd181f9a37c342e6771e32740c926296dbb42d7
Sonam Datok <datoksonam@gmail.com>
2023-11-03 16:52:10 -0400

    Create ScreenRecorder.jsx and install react router

--- src/AudioRecorder.jsx
+++ src/AudioRecorder.jsx
@@ -1,17 +1,29 @@
 import { useState, useRef } from "react";
+
+const mimeType = "audio/webm";
+
 const AudioRecorder = () => {
     const [permission, setPermission] = useState(false);
+
+    const mediaRecorder = useRef(null);
+
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
+
     const [stream, setStream] = useState(null);
+
+    const [audio, setAudio] = useState(null);
+
+    const [audioChunks, setAudioChunks] = useState([]);
 
     const getMicrophonePermission = async () => {
         if ("MediaRecorder" in window) {
             try {
-                const streamData = await navigator.mediaDevices.getUserMedia({
+                const mediaStream = await navigator.mediaDevices.getUserMedia({
                     audio: true,
                     video: false,
                 });
                 setPermission(true);
-                setStream(streamData);
+                setStream(mediaStream);
             } catch (err) {
                 alert(err.message);
             }
@@ -19,6 +31,40 @@ const AudioRecorder = () => {
             alert("The MediaRecorder API is not supported in your browser.");
         }
     };
+
+    const startRecording = async () => {
+        setRecordingStatus("recording");
+        const media = new MediaRecorder(stream, { type: mimeType });
+
+        mediaRecorder.current = media;
+
+        mediaRecorder.current.start();
+
+        let localAudioChunks = [];
+
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === "undefined") return;
+            if (event.data.size === 0) return;
+            localAudioChunks.push(event.data);
+        };
+
+        setAudioChunks(localAudioChunks);
+    };
+
+    const stopRecording = () => {
+        setRecordingStatus("inactive");
+        mediaRecorder.current.stop();
+
+        mediaRecorder.current.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: mimeType });
+            const audioUrl = URL.createObjectURL(audioBlob);
+
+            setAudio(audioUrl);
+
+            setAudioChunks([]);
+        };
+    };
+
     return (
         <div>
             <h2>Audio Recorder</h2>
@@ -28,15 +74,29 @@ const AudioRecorder = () => {
                         <button onClick={getMicrophonePermission} type="button">
                             Get Microphone
                         </button>
-                    ): null}
-                    {permission ? (
-                        <button type="button">
-                            Record
+                    ) : null}
+                    {permission && recordingStatus === "inactive" ? (
+                        <button onClick={startRecording} type="button">
+                            Start Recording
                         </button>
-                    ): null}
+                    ) : null}
+                    {recordingStatus === "recording" ? (
+                        <button onClick={stopRecording} type="button">
+                            Stop Recording
+                        </button>
+                    ) : null}
                 </div>
+                {audio ? (
+                    <div className="audio-player">
+                        <audio src={audio} controls></audio>
+                        <a download href={audio}>
+                            Download Recording
+                        </a>
+                    </div>
+                ) : null}
             </main>
         </div>
     );
 };
+
 export default AudioRecorder;
